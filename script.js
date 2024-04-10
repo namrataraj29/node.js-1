19cda78e7b7b4b1264b0ce6040c6b9586d732bc7
namratraj29 <namrataraj2926@.com>
2024-04-10 17:26:50 +0530

    added

---
+++ script.js
@@ -0,0 +1,23 @@
+// let n=5;
+// for(let i=0;i<n;i++){
+//     console.log("hello,",i)
+// }
+// console.log("bye")
+
+// console.log(process.argv)
+// let arg=process.argv
+
+// for(let i=2;i<arg.length;i++){
+//     console.log("hello to",arg[i])
+// }
+
+// const math= require("./math")
+
+// console.log(math)
+
+// const info = require("./fruits")
+// console.log(info)
+
+import {sum,pi} from "./math.js"
+
+console.log(sum(1,2))
